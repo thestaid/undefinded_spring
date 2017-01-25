b1f376ea8aafe37eddc21525e702d79d96b1441f
thestaid <thestaid@naver.com>
2017-01-25 17:10:31 +0900

    Unde3th spring project basic setting 17/01/25

---
+++ Undefined3th_SpringPro/src/main/java/com/spring/unde/HomeController.java
@@ -0,0 +1,18 @@
+package com.spring.unde;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	@RequestMapping("home.do")
+	public String home() {
+		return "home";
+	}
+	
+}
